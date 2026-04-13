6d857baea6b99868f178256fcd746ed48a0975a9
Jeslyn <jeslyn.sorotsky@gmail.com>
2026-04-12 23:10:58 -0400

    fixing conflicts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -43,7 +43,7 @@ export default function Home(){
                     <button className="btn" onClick={() => handleMoodSelect('/sad')}> Sad </button>
                     <button className="btn" onClick={() => handleMoodSelect('/happy')}> Happy </button>
                     <button className="btn" onClick={() => handleMoodSelect('/stressed')}> Stressed </button>
-                    <button className="btn" onClick={() => handleMoodSelect('/normal')}> Tired </button>
+                    <button className="btn" onClick={() => handleMoodSelect('/angry')}> Angry </button>
                     <button className="btn" onClick={() => handleMoodSelect('/tired')}> Tired </button>
 
                 </div>
